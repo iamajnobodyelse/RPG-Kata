e579c421d5b1eb6d1437c981705478c53f71cac5
iamajnobodyelse <iamajnobodyelse@gmail.com>
2010-10-13 18:51:28 -0700

    first commit

---
+++ RPG Kata/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RPG_Kata
+{
+
+    class Program
+    {
+
+        private int RandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
